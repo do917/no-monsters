77dce7b88995630d1a82f193d9b0f477799e8b55
David Oh <david.oh@underarmour.com>
2018-04-19 16:16:27 -0700

    add navigation bar

--- app/components/Home.js
+++ app/components/Home.js
@@ -12,11 +12,37 @@ export default class Home extends Component<Props> {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
-          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
-            Aye
-          </button>
+          <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+            <header className="mdl-layout__header">
+              <div className="mdl-layout__header-row">
+                <span className="mdl-layout-title">Home</span>
+
+                <div className="mdl-layout-spacer" />
+
+                <nav className="mdl-navigation mdl-layout--large-screen-only">
+                  <a className="mdl-navigation__link" href="">Link</a>
+                  <a className="mdl-navigation__link" href="">Link</a>
+                  <a className="mdl-navigation__link" href="">Link</a>
+                  <a className="mdl-navigation__link" href="">Link</a>
+                </nav>
+              </div>
+            </header>
+            <div className="mdl-layout__drawer">
+              <span className="mdl-layout-title">Title</span>
+              <nav className="mdl-navigation">
+                <a className="mdl-navigation__link" href="">Link</a>
+                <a className="mdl-navigation__link" href="">Link</a>
+                <a className="mdl-navigation__link" href="">Link</a>
+                <a className="mdl-navigation__link" href="">Link</a>
+              </nav>
+            </div>
+
+            <main className="mdl-layout__content">
+              <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+                Login
+              </button>
+            </main>
+          </div>
         </div>
       </div>
     );
